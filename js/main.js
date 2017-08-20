69aa20dbe93f839cd5e09b79b4c4d23d10b7fb0b
alfacoy <ninanbudevelop@gmail.com>
2017-08-20 10:51:41 -0300

    Modelo de cajas.
    *Enlazamos el JS al HTML.
    *Construimos el array multidimensional.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//VARIABLES DEL MAPEADO.
+//Linea numero uno
+  var a = document.querySelector('#box0');
+  var b = document.querySelector('#box1');
+  var c = document.querySelector('#box2');
+
+//Linea numero dos
+  var d = document.querySelector('#box3');
+  var e = document.querySelector('#box4');
+  var f = document.querySelector('#box5');
+
+//Linea numero tres
+  var g = document.querySelector('#box6');
+  var h = document.querySelector('#box7');
+  var i = document.querySelector('#box8');
+
+
+  var casillas = new Array(9);
+    casillas = [ [a,b,c],[d,e,f],[g,h,i] ];
+
+//FIN VARIABLES DEL MAPEADO.
+
+
+
+//CONSOLA.
+  const consola = document.querySelector('#consola').innerHTML="Esperando interaccion."
+//FIN Consola.
